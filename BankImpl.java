338fa26039ea0f737fadd2d12f262d5496f5b419
Doha Ali <alialidddd4@gmail.com>
2022-07-08 00:17:10 -0700

    Update Code

--- BankImpl.java
+++ BankImpl.java
@@ -1,28 +1,212 @@
 package Banker;
 
+
 /**
  * The Bank
  */
+
+import static banker.Customer.COUNT; 
+
+import static banker.Factory.*; 
+
+import java.util.*; 
+
 public class BankImpl implements Bank {
+	
+int[][] maximum = new int[COUNT][numOfResources]; 
+
+    int[][] need = new int[COUNT][numOfResources] ; 
+
+    int[][] needTemp = new int[COUNT][numOfResources] ; 
+
+    int[] available = new int[numOfResources]; 
+
+    int[] work = new int[numOfResources]; 
+
+     
+
+    int[][] allocTemp = new int[COUNT][numOfResources]; 
+
+    boolean[] finish = new boolean[COUNT]; 
+
+BankImpl(int[] resources) { 
+
+        // intialize for available resources  
+
+      for (int i = 0; i < resources.length; i++) { 
+
+            available[i] = resources[i]; 
+
+            work[i] = available[i];   // work matrix 
+
+        } 
+
+     } 
+
 
     @Override
     public void addCustomer(int threadNum, int[] maxDemand) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+	//Create the workers array  
+        workers[threadNum] =new Thread(new Customer(threadNum,maxDemand,theBank)); 
     }
+
+
+ private void computeNeed(){ 
+        // need matrix && need temp matrix && allocTemp 
+
+     	 for (int i = 0; i < COUNT; i++) { 
+
+            for (int j = 0; j < numOfResources; j++) { 
+
+                need[i][j] = maxArr[i][j] - allocArr[i][j]; 
+
+                needTemp[i][j] = maxArr[i][j] - allocArr[i][j]; 
+
+                allocTemp[i][j] = allocArr[i][j]; 
+
+            } 
+
+      } 
+
+    } 
 
     @Override
     public void getState() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         for (int i = 0; i < finish.length; i++) { 
+
+            finish[i] = false; 
+
+        } 
+
+        for (int i = 0; i < finish.length; i++) { 
+
+            if(finish[i] == false || needTemp[threadNum][i] <= work[i]){ 
+
+                work[i] = work[i] + allocTemp[threadNum][i]; 
+
+                finish[i] = true; 
+
+            } 
+
+        } 
+
+        for (int i = 0; i < finish.length; i++) { 
+
+            if(finish[i] == false) 
+
+                return false; // UnSafe state 
+
+        } 
+
+        return true; // safe state 
     }
 
     @Override
     public boolean requestResources(int threadNum, int[] request) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+	 computeNeed(); 
+
+      System.out.println("Customer " + threadNum + " request " + Arrays.toString(request) + " and the available is " + Arrays.toString(available)); 
+
+     for (int i = 0; i < request.length; i++) { 
+
+            if(request[i] > need[threadNum][i]){ 
+
+                System.out.println("Customer " +threadNum + " has exceeded its need claim " + 	Arrays.toString(need[threadNum]) 
+
+                + " So the request is denied"); 
+
+                return false; 
+
+            } 
+
+        } 
+
+        for (int i = 0; i < request.length; i++) { 
+
+            if(request[i] > available[i]){ 
+
+                System.out.println("Customer " + threadNum + " must wait, since resources are not available "); 
+
+                return false; 
+
+            } 
+
+        } 
+
+   	 boolean state = pretend(threadNum,request); 
+		
+   	 if(state == true){  // Safe State 
+
+            for (int i = 0; i < available.length; i++) { 
+
+                available[i] = work[i]; 
+
+            } 
+
+            for (int i = 0; i < COUNT; i++) { 
+
+                for (int j = 0; j < numOfResources; j++) { 
+
+                    need[i][j] = needTemp[i][j]; 
+
+                    allocArr[i][j] = allocTemp[i][j]; 
+
+                } 
+
+   	     } 
+
+            System.out.println("The request "+ Arrays.toString(request) + " of customer " + threadNum 
+
+                    +" will make the system in SAFE state , so it's Done"); 
+
+            return true; 
+
+  	 }else{  // UnSafe state 
+
+            System.out.println("The request "+ Arrays.toString(request) + " of customer " + threadNum + 
+
+                    " will make the System in UNSAFE state so , customer " + threadNum + " will take a nap"); 
+
+        	return false; 
+
+        } 
     }
+
+	private boolean pretend(int threadNum,int[] request){ 
+
+        	for (int i = 0; i < request.length; i++) { 
+
+          	  work[i] = work[i] - request[i]; 
+
+           	 allocTemp[threadNum][i] = allocTemp[threadNum][i] + request[i];  
+
+            	needTemp[threadNum][i] = needTemp[threadNum][i] - request[i]; 
+
+        } 
+
+	boolean state = this.getState(threadNum);  // return the state of the system 
+
+        return state; 
+
+    } 
 
     @Override
     public void releaseResources(int threadNum, int[] release) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (int i = 0; i < numOfResources; i++) { 
+
+            available[i] = allocArr[threadNum][i] + realese[i]; 
+
+        } 
+
+        System.out.println("Customer " + threadNum + " realsing " + Arrays.toString(realese)  
+
+                + " , available = " + Arrays.toString(available) + 
+
+                " , Allocated = " + Arrays.deepToString(allocArr)); 
+
+    	
     }
 
 }
--- Factory.java
+++ Factory.java
@@ -14,67 +14,112 @@ import java.util.*;
 
 public class Factory
 {
+	public static int numOfResources = 3; 
+
+    	public static int allocArr[][] = new int[Customer.COUNT][numOfResources]; 
+
+   	public static int[] maxDemand = new int[numOfResources]; 
+
+   	public static int maxArr[][] = new int[Customer.COUNT][numOfResources]; 
+    	
+	public static Thread[] workers = new Thread[Customer.COUNT]; 
+
+   	public static Bank theBank = null; 
+
 	public static void main(String[] args) {
-                int numOfResources; 
-                int[] resources;                
-                if(args.length == 0){
-                    numOfResources = 3;
-                    resources = new int[]{10, 5, 7};
-                }else{
-                    numOfResources = args.length;
-                    resources = new int[numOfResources];
-                    for (int i = 0; i < numOfResources; i++)
-                            resources[i] = Integer.parseInt(args[i].trim());
-                }
-                
-		Bank theBank = new BankImpl(resources);
-                int[] maxDemand = new int[numOfResources];
-                
-                // the customers
-                Thread[] workers = new Thread[Customer.COUNT];
-                
-		// read initial values for maximum array 
-		String line;
-		try {
-			BufferedReader inFile = new BufferedReader(new FileReader("./src/Banker/infile.txt"));
+	
+	Scanner sc = new Scanner(System.in);  
 
-			int threadNum = 0;
-                        int resourceNum = 0;
+        int[] resources;                 
 
-                        for (int i = 0; i < Customer.COUNT; i++) {
-                            line = inFile.readLine();
-                            StringTokenizer tokens = new StringTokenizer(line,",");
-                            
-                            while (tokens.hasMoreTokens()) {
-                                int amt = Integer.parseInt(tokens.nextToken().trim());
-                                maxDemand[resourceNum++] = amt;
-                            }
-                            workers[threadNum] = new Thread(new Customer(threadNum, maxDemand, theBank));
-                            theBank.addCustomer(threadNum,maxDemand);
-                            //theBank.getCustomer(threadNum);
-                            ++threadNum;
-                            resourceNum = 0;
-                        }
-                }
-		catch (FileNotFoundException fnfe) {
-			throw new Error("Unable to find file \"infile.txt\"");
-		}
-		catch (IOException ioe) {
-			throw new Error("Error processing \"infile.txt\"");
-		}
-                
-                // start all the customers
-                System.out.println("FACTORY: created threads");
+        if(args.length == 0){ 
 
-                for (int i = 0; i < Customer.COUNT; i++) 
-                    workers[i].start();
-                    
-                System.out.println("FACTORY: started threads");
+            resources = new int[]{10, 5, 7}; 
 
-                try { Thread.sleep(10000); } catch (InterruptedException ie) { }
-                System.out.println("FACTORY: interrupting threads");
-                for (int i = 0; i < Customer.COUNT; i++)
-                    workers[i].interrupt();
+        }else{ 
 
-	}
+            numOfResources = args.length; 
+
+            resources = new int[numOfResources]; 
+
+            for (int i = 0; i < numOfResources; i++) 
+
+                 resources[i] = Integer.parseInt(args[i].trim()); 
+
+        } 
+
+        theBank = new BankImpl(resources); 
+
+                /* Allocation Array. */ 
+
+        for (int i = 0; i < Customer.COUNT; i++) { 
+
+            for (int j = 0; j < numOfResources; j++) { 
+
+                /* Enter allocation of instances of resources. */ 
+
+                System.out.println("enter allocation instances of resources: " + j + " for process p" + i); 
+
+                /* Enter allocation of instances of resources. */ 
+
+                allocArr[i][j] = sc.nextInt(); 
+
+            } 
+
+        } 
+
+        // start all the customers 
+
+        System.out.println("FACTORY: created threads"); 
+
+        // read initial values for maximum array  
+
+        System.out.println("enter maximum of resources: "); 
+
+        for (int i = 0; i < Customer.COUNT; i++) { 
+
+            for (int j = 0; j < numOfResources; j++) { 
+
+                System.out.println("enter max instances of resources:" + j + "for process p" + i); 
+
+                /* Enter max instances of resources for a given resource. */ 
+
+                int number = sc.nextInt(); 
+
+                while(number > resources[j]){ 
+
+                    System.out.println("Invalid input as it greater than the maximum " + resources[j]); 
+
+                    number = sc.nextInt(); 
+               } 
+
+                maxArr[i][j] = number; 
+
+                maxDemand[j] = number; 
+
+            } 
+
+            theBank.addCustomer(i, maxDemand); 
+        } 
+
+        System.out.println("FACTORY: started threads"); 
+
+        for (int i = 0; i < Customer.COUNT; i++) { 
+
+            workers[i].start(); 
+        } 
+
+        try { 
+		Thread.sleep(10000); 
+	} catch(InterruptedException ie) { } 
+
+             
+        System.out.println("FACTORY: interrupting threads"); 
+
+        for (int i = 0; i < Customer.COUNT; i++) 
+            workers[i].interrupt(); 
+
+} 
+                 
+              
 }
